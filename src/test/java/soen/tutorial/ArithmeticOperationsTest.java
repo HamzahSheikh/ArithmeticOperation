c04af9b8ac9f0bde46ecc5bb6a97c2f87fa58334
Hamzah Sheikh <57862914+HamzahSheikh@users.noreply.github.com>
2022-03-30 18:26:50 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -3,17 +3,29 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
-public class ArithmeticOperationsTest 
-{
+public class ArithmeticOperationsTest {
+
+
+	@Test
+	public void testAdd()
+	{
 
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		//assertEquals(expected, actual);
-		fail("must fail");
+		assertEquals(expected, actual);
 
-	
+	}
+    @Test
+    public void testSub()
+    {
+
+        ArithmeticOperations operations = new ArithmeticOperations();
+        Integer actual = operations.addOrSub(6, 2);
+        Integer expected = 4;
+        assertEquals(expected, actual);
+
+    }
+
 }
-
